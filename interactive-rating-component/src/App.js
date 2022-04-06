0bc4ed93d93a2783bea027fc55b6374eab5f229a
CaffeineProgrammer <crsph@users.noreply.github.com>
2022-04-06 23:12:15 +0200

    WIP

--- interactive-rating-component/src/App.js
+++ interactive-rating-component/src/App.js
@@ -1,8 +1,15 @@
-import './App.scss';
+import "./App.scss";
+import CircleButton from "./components/CircleButton";
+
+let numbers = [1, 2, 3, 4, 5];
 
 function App() {
   return (
-    <div class="rating-container" />
+    <div class="rating-container">
+      {numbers.map((number) => {
+        <CircleButton number={number} />;
+      })}
+    </div>
   );
 }
 
