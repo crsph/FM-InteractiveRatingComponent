279dfd7c347865364892aacb19e0aeaccbc4be6d
CaffeineProgrammer <crsph@users.noreply.github.com>
2022-04-07 14:51:37 +0200

    Added conditional rendering between DOM elements

--- interactive-rating-component/src/components/CircleButton.js
+++ interactive-rating-component/src/components/CircleButton.js
@@ -2,9 +2,13 @@ import React from "react";
 import "./CircleButton.scss";
 
 export default function CircleButton(props) {
-  return (
-    <div class="button-container">
-      <b>{props.number}</b>
-    </div>
-  );
+  let button;
+
+  if (props.category === "number") {
+    button = <b>{props.number}</b>;
+  } else if (props.category === "image") {
+    button = <img src={props.src} alt="Star Logo" />;
+  }
+
+  return <div class="button-container">{button}</div>;
 }
