293597be1da594b2226fbe71dcdecf840d91b399
CaffeineProgrammer <crsph@users.noreply.github.com>
2022-04-08 11:03:10 +0200

    Relocated content from App.js to RatingView.js

--- interactive-rating-component/src/App.js
+++ interactive-rating-component/src/App.js
@@ -1,32 +1,8 @@
 import "./App.scss";
-import CircleButton from "./components/CircleButton";
-import SubmitButton from "./components/SubmitButton";
-import StarLogo from "./assets/icon-star.svg";
-
-const numbers = [1, 2, 3, 4, 5];
+import RatingView from "./views/RatingView";
 
 function App() {
-  return (
-    <div class="rating-container">
-      <CircleButton category={"image"} src={StarLogo} />
-
-      <h1 class="rating-container__title">How did we do?</h1>
-
-      <div class="rating-container__description">
-        Please let us know how we did with your support request. All feedback is
-        appreciated to help us improve our offering!
-      </div>
-
-      <div class="fluid-button-container">
-        {numbers.map((number) => {
-          console.log(number);
-          return <CircleButton category={"number"} number={number} />;
-        })}
-      </div>
-
-      <SubmitButton />
-    </div>
-  );
+  return <RatingView />;
 }
 
 export default App;
