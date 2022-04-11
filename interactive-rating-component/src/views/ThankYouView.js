d7c7887a10bc66f46f257d1696a62e371f250073
CaffeineProgrammer <crsph@users.noreply.github.com>
2022-04-11 12:25:38 +0200

    Update ThankYouView.js

--- interactive-rating-component/src/views/ThankYouView.js
+++ interactive-rating-component/src/views/ThankYouView.js
@@ -1,10 +1,28 @@
 import React from "react";
+import ThankYouIllu from "../assets/illustration-thank-you.svg";
 import { useLocation } from "react-router-dom";
+import "./ThankYouView.scss";
 
 export default function ThankYouView() {
   const location = useLocation();
 
-  console.log(location.state);
+  return (
+    <div className="thank-you-container">
+      <div className="image-container">
+        <img src={ThankYouIllu} alt="Thank You Illustration" />
+      </div>
 
-  return <div>ThankYouView</div>;
+      <div className="thank-you-container__selected-rate-number">
+        You selected {location.state} out of 5
+      </div>
+
+      <div className="text-container">
+        <h1 className="text-container__title">Thank You!</h1>
+        <div className="text-container__description">
+          We appreciate you taking the time to give a rating. If you ever need
+          more support, don't hesitate to get in touch!
+        </div>
+      </div>
+    </div>
+  );
 }
