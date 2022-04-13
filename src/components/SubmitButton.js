ba31faf626547af2ce5f0c781b77de09a7818d0b
CaffeineProgrammer <crsph@users.noreply.github.com>
2022-04-13 11:11:57 +0200

    Added disable functionality on submit button

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./SubmitButton.scss";
 
+let submitButton;
+
 export default function SubmitButton(props) {
-  return (
-    <button onClick={props.onClick} className="submit-button-container">
-      SUBMIT
-    </button>
-  );
+  if (props.disabled === true) {
+    submitButton = (
+      <button
+        disabled
+        onClick={props.onClick}
+        className="submit-button-container"
+      >
+        SUBMIT
+      </button>
+    );
+  } else {
+    submitButton = (
+      <button onClick={props.onClick} className="submit-button-container">
+        SUBMIT
+      </button>
+    );
+  }
+
+  return <>{submitButton}</>;
 }
